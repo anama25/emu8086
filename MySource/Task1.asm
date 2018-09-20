c00ba530989e71ece8089ddd6bfae07452d62cb5
anama25 <42063254+anama25@users.noreply.github.com>
2018-09-20 15:10:20 +0800

    Update and rename file.asm to Task1.asm

---
+++ MySource/Task1.asm
@@ -0,0 +1,17 @@
+; Author: 
+; Date: 9/2018
+.586
+.MODEL FLAT
+.STACK 4096 			; reserve 4096-byte stack
+.DATA 				; reserve storage for data
+number DWORD -105
+sum DWORD ?
+.CODE 				; start of main program code
+main PROC
+ mov eax, number 		; first number to EAX
+ add eax, 158 			; add 158
+ mov sum, eax 			; sum to memory
+ mov eax, 0 			; exit with return code 0
+ ret
+main ENDP
+END 				; end of source code
